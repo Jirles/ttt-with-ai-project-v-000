b075036095db06ba9e51df1b73778ac2599dbf3c
Emily Jirles <emily.jirles@gmail.com>
2018-05-06 00:42:43 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -11,7 +11,6 @@ module Players
     CORNERS = ["1", "3", "7", "9"]
 
     def initialize(level_of_difficulty=2, token)
-      super
       @cheat_sheet = [["1","2","3"], ["4","5","6"], ["7","8","9"], ["1","4","7"], ["2","5","8"], ["3","6","9"], ["1","5","9"], ["3","5","7"]] #=> copy of WIN_COMBINATIONS but as valid moves 
       
       #determines if you play stupid computer (random sampling) or ai 
