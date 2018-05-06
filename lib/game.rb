8e844d99b5bad0f96e9b50668298ed09e884d836
Emily Jirles <emily.jirles@gmail.com>
2018-05-06 00:15:27 +0000

    automatically backed up by learn

---
+++ lib/game.rb
@@ -0,0 +1,114 @@
+
+class Game
+  
+  WIN_COMBINATIONS = [
+    [0,1,2], #top row
+    [3,4,5], #mid row
+    [6,7,8], #bot row
+    [0,3,6], #left col
+    [1,4,7], #mid col
+    [2,5,8], #right col
+    [0,4,8], #l>r diag
+    [2,4,6]  #r>l diag
+  ]
+
+  attr_accessor :board, :player_1, :player_2
+  
+  def initialize(player_1=Players::Human.new('X'), player_2=Players::Human.new('O'), board=Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board 
+  end
+  
+  def self.start
+    play = true 
+    while play
+      puts "Welcome to TicTacToe!"
+      puts "How many players?"
+      puts "Please enter 0 for a 0-player game, where the computer plays itself,"
+      puts "Please enter 1 to play against the computer."
+      puts "Please enter 2 to play against a friend."
+      player_num = gets.chomp 
+      if player_num == "0"
+        game = Game.new(player_1=Players::Computer.new('X'), player_2=Players::Computer.new('O'))
+      elsif player_num == "1"
+        puts "Who will go first? Press c for the computer or h for human."
+        player1 = gets.chomp 
+        if player1 == 'c'
+         game = Game.new(player_1=Players::Computer.new('X'), player_2=Players::Human.new('O'))
+        else 
+          game = Game.new(player_1=Players::Human.new('X'), player_2=Players::Computer.new('O'))
+        end
+      else 
+        game = Game.new 
+      end
+      puts "Let's play!"
+      game.play 
+      puts "Nice job! Would you like to play again? y/n"
+      input = gets.chomp 
+      input == 'y' ? play : play = false 
+    end
+    puts "Buh bye!"
+  end
+  
+  # returns currently player based on number of turns already taken 
+  # board object tracks turns already taken 
+  def current_player
+    self.board.turn_count.even? ? self.player_1 : self.player_2
+  end
+
+  # Validation methods:
+  
+  # checks WIN_COMBINATIONS and board too look for a winner, returns winning combination
+  def won?
+    winner = nil
+    WIN_COMBINATIONS.each do |combo|
+      converted_combo = combo.collect{ |i| self.board.cells[i] }
+      if converted_combo.all?{|cell| cell == "X"} || converted_combo.all?{|cell| cell == "O"}
+        winner = combo 
+      end
+    end
+    winner 
+  end
+
+
+  #are all the spaces full without a winning combination?
+  def draw?
+    !self.won? && self.board.full?
+  end
+
+  #game over?
+  def over?
+    self.won? || self.draw?
+  end
+
+  # checks to see who is the winner, returns X, O, or nil
+  # if there is a winner, 
+  # use won? (returns the winning combo from WIN_COMBINATIONS) to index into board cells
+  # else nil 
+  def winner
+    self.won? ? self.board.cells[self.won?[0]] : nil 
+  end
+
+  def turn
+    move = self.current_player.move(self.board)
+    until self.board.valid_move?(move)
+      move = self.current_player.move(self.board)
+    end
+    self.board.update(move, self.current_player)
+  end
+    
+
+  def play
+    until self.over?
+      self.board.display
+      self.turn
+    end
+    if self.won?
+      puts "Congratulations #{self.winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
+
+end
---
+++ lib/players/computer.rb
@@ -0,0 +1,111 @@
+module Players
+
+  class Computer < Player
+    
+    attr_accessor :cheat_sheet 
+    attr_reader :token, :level_of_difficulty, :opponent_token
+    
+   # WIN_HASH = {"1"=>3, "2"=>2, "3"=>3, "4"=>2, "5"=>4, "6"=>2, "7"=>3, "8"=>2, "9"=>3}
+    CENTER = "5"
+    CORNERS = ["1", "3", "7", "9"]
+
+    def initialize(level_of_difficulty=2, token)
+      @cheat_sheet = [["1","2","3"], ["4","5","6"], ["7","8","9"], ["1","4","7"], ["2","5","8"], ["3","6","9"], ["1","5","9"], ["3","5","7"]] #=> copy of WIN_COMBINATIONS but as valid moves 
+      
+      #determines if you play stupid computer (random sampling) or ai 
+      @level_of_difficulty = level_of_difficulty
+      @token = token 
+      case 
+        when token ==  "X"
+          @opponent_token = "O"
+        else
+          @opponent_token = "X"
+      end
+    end
+  
+    # dumb computer (1) passes all tests - success
+    
+    def random_move(board)
+      # just choose something at random until you hit upon a valid move
+      (1..9).to_a.sample.to_s
+    end
+    
+    # begin smart computer (2) methods - not passing tests
+    
+    def update_cheat_sheet(board)
+      #iterate over cheat_sheet
+      cheat_sheet.collect do |combo|
+        # go through each combo so we can look at each string (move) in the combo array
+        combo.collect do |move|
+          
+          # that move once converted to an integer and subtracted 1 becomes an index
+            index = move.to_i - 1
+            
+          # the index can be used to access the board.cells property to find out what is present
+          # if there is nothing, i.e. " " at board.cells[index]
+          if board.cells[index] == " "
+            
+            # then nothing happens to that string
+            move 
+            
+          # else that string is replaced by board.cells[index] ("X" or "O")
+          else
+            board.cells[index] 
+          end 
+        end 
+      end 
+    end 
+
+    #determining opponent_token is necessary for move-choosing-methods block? and win_imminent?
+
+    #CENTER has most WIN_COMBINATIONS but if it is taken, an opposing token in one of the CORNERS will ensure a cat's game or possibly a win with a stupid enough opponent
+    def first_move(board)
+      if !board.taken?(CENTER)
+        CENTER
+      else
+        CORNERS.sample #<= chooses randomly from CORNERS
+      end
+    end
+
+    #determines if a win is imminent, i.e. there is a WIN_COMBINATION with two spaces taken by sef.token and an empty space (will be an integer on cheat_sheet)
+
+  def win_imminent?
+    cheat_sheet.detect do |combo| 
+      combo.count{|space| space == token} == 2 && !combo.include?(opponent_token)
+    end
+  end
+
+    # uses cheat_sheet and determines if a block is necessary if a combo contains two opponent_tokens 
+    
+    def block?
+      cheat_sheet.detect do |combo| 
+        combo.count{|space| space == opponent_token} == 2 && !(combo.include?(token))
+      end
+    end
+    
+    def ai_move(board)
+      cheat_sheet = update_cheat_sheet(board)
+      if board.turn_count == 0 || board.turn_count == 1
+        first_move(board)
+      else
+        if win_imminent?
+            win_imminent?.detect{|x| x != token}
+        elsif block? 
+            block?.detect{|x| x != opponent_token}
+        else
+            random_move(board)
+        end
+      end
+    end
+    
+    def move(board)
+      if level_of_difficulty == 1
+        random_move
+      else
+        ai_move(board)
+      end
+    end
+
+  end
+  
+end
